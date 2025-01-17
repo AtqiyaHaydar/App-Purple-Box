788d9d23260095ee55d34bb7512baf1edc491636
PurpleBoxTeam <paolo@purple-box.app>
2025-01-18 02:45:24 +0700

    fix(i18n): enhance locale detection by using accept-language header and add timeZone to response

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,27 +1,30 @@
-import { getRequestConfig, GetRequestConfigParams } from "next-intl/server";
+import { getRequestConfig } from "next-intl/server";
+import { headers } from "next/headers";
 
-export default getRequestConfig(
-  async ({ requestLocale }: GetRequestConfigParams) => {
-    const defaultLocale = "en";
-    const supportedLocales = ["en", "fr", "es", "it", "id", "pt", "de", "ae"];
+export default getRequestConfig(async () => {
+  const defaultLocale = "en";
+  const supportedLocales = ["en", "fr", "es", "it", "id", "pt", "de", "ae"];
 
-    // Get the locale from the Accept-Language header
-    const acceptLanguage = await requestLocale;
-    let locale = defaultLocale;
+  const headersList = headers();
+  const acceptLanguage = headersList.get("accept-language");
 
-    if (acceptLanguage) {
-      const browserLocales = acceptLanguage
-        .split(",")
-        .map((lang) => lang.split(";")[0].trim());
+  let locale = defaultLocale;
 
-      locale =
-        browserLocales.find((lang) => supportedLocales.includes(lang)) ||
-        defaultLocale;
-    }
+  if (acceptLanguage) {
+    const browserLocales = acceptLanguage
+      .split(",")
+      .map((lang) => lang.split(";")[0].trim().split("-")[0]);
 
-    return {
-      locale,
-      messages: (await import(`../messages/${locale}.json`)).default,
-    };
+    console.log(browserLocales);
+
+    locale =
+      browserLocales.find((lang) => supportedLocales.includes(lang)) ||
+      defaultLocale;
   }
-);
+
+  return {
+    locale,
+    messages: (await import(`../messages/${locale}.json`)).default,
+    timeZone: "Europe/Rome",
+  };
+});
